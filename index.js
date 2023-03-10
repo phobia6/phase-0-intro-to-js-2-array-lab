6d5d70586c7d9c38f6feb198939ea6d255e0f020
TohilGuerra <tohil6@gmail.com>
2023-03-10 17:37:48 -0600

    add arrays

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function reset() {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+}
+
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+}
+reset();
+cats; 
+
+function destructivelyPrependCat() {
+  cats.unshift("Bob");
+}
+reset();
+cats;
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+reset();
+cats;
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+reset();
+cats;
+
+function appendCat(name) {
+  return [...cats,name];
+}
+appendCat("Broom");
+
+function prependCat(name) {
+  return [name,...cats];
+}
+prependCat("Broom");
+
+function removeLastCat() {
+  return [...cats.slice(0,2)];
+}
+removeLastCat();
+
+function removeFirstCat() {
+  return [...cats.slice(-2)]
+}
+removeFirstCat();
